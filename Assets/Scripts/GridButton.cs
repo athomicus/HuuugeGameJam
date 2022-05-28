f73e91288b7b19394a86b16bdbad02161b90920f
tyruji <63365694+tyruji@users.noreply.github.com>
2022-05-28 18:49:24 +0200

    Ogarnieta siatka w miare
    mozna klikac w rycerzy, podswietlaja sie kratki i sie ruszaja rycerze

---
+++ Assets/Scripts/GridButton.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridButton : Button
+{
+    public Vector3 WorldPosition => transform.position;
+
+    public Vector2Int GridPosition { get; set; } = Vector2Int.zero;
+
+    public OccupyingObject ObjectAtButton { get; set; } = null;
+
+    public GridButton[,] Grid { get; set; } = null;
+
+    private static OccupyingObject _selectedObject = null;
+
+    public void ClickButton()
+    {
+            // We click an empty field.
+        if( _selectedObject == null && ObjectAtButton == null ) return;
+
+        // We tried to move the knight at the same position it is in,
+        // so deselect the knight and unhighlight the available buttons.
+        if( _selectedObject == ObjectAtButton )
+        {
+            UnhighlightAvailableButtons();
+
+            _selectedObject = null;
+
+            return;
+        }
+
+        // We move a selected knight
+        if( _selectedObject != null && ObjectAtButton == null )
+        {
+            UnhighlightAvailableButtons();
+
+            ObjectAtButton = _selectedObject;
+            var pos = ObjectAtButton.gridPosition;
+            Grid[ pos.x, pos.y ].ObjectAtButton = null;
+            ObjectAtButton.gridPosition = GridPosition;
+            _selectedObject = null;
+
+            // TODO: start moving the knight, playing the animation...
+
+            ObjectAtButton.gameObject.transform.position = WorldPosition;
+
+            return;
+        }
+
+            // We select a knight
+        if( _selectedObject == null )
+        {
+            _selectedObject = ObjectAtButton;
+
+            HighlightAvailableButtons();
+
+            return;
+        }
+    }
+
+    private void SetButtonColour( Vector2Int pos, Color colour )
+    {
+        Grid[ pos.x, pos.y ].image.color = colour;
+    }
+
+    private void HighlightAvailableButtons()
+    {
+        for( int i = GridPosition.x - 1; i <= GridPosition.x + 1; ++i )
+        {
+            for( int j = GridPosition.y - 1; j <= GridPosition.y + 1; ++j )
+            {
+                    // If the indeces are out of bounds, skip them.
+                if( i < 0 || j < 0 ) continue;
+                if( i >= Grid.GetLength( 0 ) || j >= Grid.GetLength( 1 ) ) continue;
+
+                var pos = new Vector2Int( i, j );
+
+                if( Grid[ i, j ].ObjectAtButton != null && pos != GridPosition ) continue;
+
+                Color colour = Color.green;
+
+                if( pos == GridPosition )
+                {
+                    colour = Color.cyan;
+                }
+
+                SetButtonColour( pos, colour );
+            }
+        }
+    }
+    private void UnhighlightAvailableButtons()
+    {
+        var grid_pos = _selectedObject.gridPosition;
+        for( int i = grid_pos.x - 1; i <= grid_pos.x + 1; ++i )
+        {
+            for( int j = grid_pos.y - 1; j <= grid_pos.y + 1; ++j )
+            {
+                // If the indeces are out of bounds, skip them.
+                if( i < 0 || j < 0 ) continue;
+                if( i >= Grid.GetLength( 0 ) || j >= Grid.GetLength( 1 ) ) continue;
+
+                SetButtonColour( new Vector2Int( i, j ), Color.white );
+            }
+        }
+    }
+
+    public class OccupyingObject
+    {
+        public Vector2Int gridPosition = Vector2Int.zero;
+        public GameObject gameObject = null;
+    }
+}
---
+++ Assets/Scripts/GridHandler.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent( typeof( GridLayoutGroup ) )]
+public class GridHandler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _KnightPrefab = null, _ArcherPrefab = null;
+
+    [SerializeField]
+    private GameObject _ButtonPrefab = null;
+
+    [SerializeField]
+    private Vector2Int _GridSize = Vector2Int.zero;
+
+    private GridLayoutGroup _gridLayoutGroup = null;
+
+    private GridButton[,] _buttonGrid = null;
+
+    private void Start()
+    {
+        _gridLayoutGroup = GetComponent<GridLayoutGroup>();
+
+        _gridLayoutGroup.constraintCount = _GridSize.x;
+
+        _buttonGrid = new GridButton[ _GridSize.x, _GridSize.y ];
+
+        SpawnButtons();
+
+        StartCoroutine( SpawnKnights() );
+    }
+
+    private void SpawnButtons()
+    {
+        for( int i = 0; i < _GridSize.x; ++i )
+        {
+            for( int j = 0; j < _GridSize.y; ++j )
+            {
+                var button = Instantiate( _ButtonPrefab ).GetComponent<GridButton>();
+
+                button.transform.SetParent( this.transform, worldPositionStays: false );
+
+                _buttonGrid[ i, j ] = button;
+
+                button.Grid = _buttonGrid;
+
+                button.GridPosition = new Vector2Int( i, j );
+
+                button.onClick.AddListener( button.ClickButton );
+
+                button.GetComponentInChildren<Text>().text = new Vector2Int( i, j ).ToString();
+            }
+        }
+    }
+
+    private IEnumerator SpawnKnights()
+    {
+            // We wait at the end of the frame because thats when the gridlayoutgroup 
+            // updates the child elements positions in the grid.
+        yield return new WaitForEndOfFrame();
+
+            // The sworded knight is spawned in the middle of the grid.
+        var knight_pos = new Vector2Int( 0, _GridSize.y / 2 );
+
+        CreateKnight( _KnightPrefab, knight_pos );
+        CreateKnight( _ArcherPrefab, knight_pos + Vector2Int.up );
+        CreateKnight( _ArcherPrefab, knight_pos + Vector2Int.down );
+
+    }
+
+    private void CreateKnight( GameObject prefab, Vector2Int grid_pos )
+    {
+        var obj = Instantiate( prefab, GetButtonPosition( grid_pos ), Quaternion.identity );
+
+        _buttonGrid[ grid_pos.x, grid_pos.y ].ObjectAtButton = new GridButton.OccupyingObject
+        {
+            gridPosition = grid_pos,
+            gameObject = obj
+        };
+    }
+
+    private Vector3 GetButtonPosition( Vector2Int pos ) => _buttonGrid[ pos.x, pos.y ].WorldPosition;
+}
